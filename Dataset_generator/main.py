1debe2fb80454f834b19e338f930b8778e22b10d
aswanthkoleri <aswanth9495@gmail.com>
2018-12-19 04:15:43 +0530

    Add basic comments on how to make dataset generator

---
+++ Dataset_generator/main.py
@@ -0,0 +1,17 @@
+def FindBestMethod(videoName,subtitleName):
+    # Do each algorithm and obtain the subtitle list for each algorithm.
+    
+    # Take the intersection of all the algorithm
+
+    # Find the algorithm which has the more percentage of sentences which is included in the intersection generated.
+
+    bestMethod=""
+    return bestMethod
+    
+def main():
+    # No of videos
+    noOfVideos=int(input())
+    videos=[]
+    for i in videos:
+        bestMethod=FindBestMethod(i.videoName,i.subtitleName)
+        print(bestMethod)
