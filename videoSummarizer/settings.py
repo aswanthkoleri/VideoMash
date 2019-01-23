e31b17e19878fbe06381fbe6e66947682a044225
aswanthkoleri <aswanth9495@gmail.com>
2019-01-23 13:58:30 +0530

    Add latest fit fixes for smooth working

--- videoSummarizer/settings.py
+++ videoSummarizer/settings.py
@@ -129,4 +129,4 @@ STATICFILES_DIRS = (
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
